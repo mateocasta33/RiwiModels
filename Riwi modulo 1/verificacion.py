f6850b8ba4e36f1bb36491054ed89ab760c30bf5
mateocasta33 <mateocasta33@gmail.com>
2025-04-24 07:29:23 -0500

    Ejercicio semana1 modulo1

---
+++ Riwi modulo 1/verificacion.py
@@ -0,0 +1,12 @@
+'''
+    Verificar si una persona puede acceder a un evento exclusivo. Para ello, debe ser mayor de edad (18 años o más) y contar con un permiso especial. Si cumple ambas condiciones, se le concederá el acceso; de lo contrario, se le denegará."
+'''
+# Variables
+edad = 17
+tiene_permiso = True
+
+# Operadores lógicos
+if edad  or tiene_permiso:
+    print("Acceso concedido ✅")
+else:
+    print("Acceso denegado ❌")
